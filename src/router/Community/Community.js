88b3e6c912637e8ca874c72be021a0e3758a23d5
konny <istjprogrammer@gmail.com>
2023-12-27 15:02:35 +0900

    [Gil] 첫번째 커

--- src/router/Community/Community.js
+++ src/router/Community/Community.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import BoardList from './List/boardlist';
+import BoardDetail from './View/boarddetail';
+import BoardWrite from './View/boardwrite';
+import BoardEdit from './View/boardedit';
+
+const routes = [
+  {
+    path: '/',
+    element: <BoardList />,
+  },
+  {
+    path: '/boarddetail',
+    element: <BoardDetail />,
+  },
+  {
+    path: '/boardwrite',
+    element: <BoardWrite/>
+  },
+  {
+    path: '/boarddetail/boardedit',
+    element: <BoardEdit/>
+  }
+];
 
 function Community() {
   return (
     <div className="Community">
-        <hr/>
-        커뮤니티 목록 출력
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Routes>
     </div>
   );
 }
